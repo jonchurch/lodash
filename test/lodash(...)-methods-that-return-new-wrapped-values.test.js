ba3bb7df2812b1139d37840ee9cd1c44d362ff61
Jon Church <me@jonchurch.com>
2023-09-10 17:50:59 -0400

    methods...new-wrapped-values.test.js, import _ from utils

--- test/lodash(...)-methods-that-return-new-wrapped-values.test.js
+++ test/lodash(...)-methods-that-return-new-wrapped-values.test.js
@@ -1,6 +1,8 @@
 import assert from 'assert'
 import lodashStable from 'lodash'
 
+import { _ } from './utils'
+
 describe('lodash(...) methods that return new wrapped values', () => {
   const funcs = [
     'castArray',

30391b76acbae2b103e0af6c9324af6d1b346d4a
Jon Church <me@jonchurch.com>
2023-09-10 04:01:38 -0400

    rm idenity.test.js

--- test/identity.test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import identity from '../identity.js'
-
-describe('identity', () => {
-  it('should return the first argument given', () => {
-    const object = { 'name': 'fred' }
-    assert.strictEqual(identity(object), object)
-  })
-})

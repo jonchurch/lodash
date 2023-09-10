4defe1b81e74e192ee86fad4f064276f9fb1ab6e
Jon Church <me@jonchurch.com>
2023-09-10 15:18:43 -0400

    countBy.test.js, import _

--- test/countBy.test.js
+++ test/countBy.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import lodashStable from 'lodash'
-import { LARGE_ARRAY_SIZE } from './utils'
+import { _, LARGE_ARRAY_SIZE } from './utils'
 import countBy from '../countBy.js'
 
 describe('countBy', () => {

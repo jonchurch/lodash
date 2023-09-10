15457c7c83859228b71717be487be70b2eabf497
Jon Church <me@jonchurch.com>
2023-09-10 17:51:49 -0400

    methods...wrapped-modified-array.test.js, import _ from utils

--- test/lodash(...)-methods-that-return-the-wrapped-modified-array.test.js
+++ test/lodash(...)-methods-that-return-the-wrapped-modified-array.test.js
@@ -1,6 +1,8 @@
 import assert from 'assert'
 import lodashStable from 'lodash'
 
+import { _ } from './utils'
+
 describe('lodash(...) methods that return the wrapped modified array', () => {
   const funcs = [
     'push',

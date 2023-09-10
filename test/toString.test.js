ea9aa79209b554256d74f02b77eb02013fd97096
Jon Church <me@jonchurch.com>
2023-09-10 01:42:08 -0400

    npm run lint -- --fix

--- test/toString.test.js
+++ test/toString.test.js
@@ -1,43 +1,41 @@
-import assert from 'assert';
-import lodashStable from 'lodash';
-import { stubString, symbol } from './utils.js';
-import toString from '../toString.js';
+import assert from 'assert'
+import lodashStable from 'lodash'
+import { stubString, symbol } from './utils.js'
+import toString from '../toString.js'
 
-describe('toString', function() {
-  it('should treat nullish values as empty strings', function() {
-    var values = [, null, undefined],
-        expected = lodashStable.map(values, stubString);
+describe('toString', () => {
+  it('should treat nullish values as empty strings', () => {
+    const values = [, null, undefined],
+      expected = lodashStable.map(values, stubString)
 
-    var actual = lodashStable.map(values, function(value, index) {
-      return index ? toString(value) : toString();
-    });
+    const actual = lodashStable.map(values, (value, index) => index ? toString(value) : toString())
 
-    assert.deepStrictEqual(actual, expected);
-  });
+    assert.deepStrictEqual(actual, expected)
+  })
 
-  it('should preserve the sign of `0`', function() {
-    var values = [-0, Object(-0), 0, Object(0)],
-        expected = ['-0', '-0', '0', '0'],
-        actual = lodashStable.map(values, toString);
+  it('should preserve the sign of `0`', () => {
+    const values = [-0, Object(-0), 0, Object(0)],
+      expected = ['-0', '-0', '0', '0'],
+      actual = lodashStable.map(values, toString)
 
-    assert.deepStrictEqual(actual, expected);
-  });
+    assert.deepStrictEqual(actual, expected)
+  })
 
-  it('should preserve the sign of `0` in an array', function() {
-    var values = [-0, Object(-0), 0, Object(0)];
-    assert.deepStrictEqual(toString(values), '-0,-0,0,0');
-  });
+  it('should preserve the sign of `0` in an array', () => {
+    const values = [-0, Object(-0), 0, Object(0)]
+    assert.deepStrictEqual(toString(values), '-0,-0,0,0')
+  })
 
-  it('should handle symbols', function() {
-    assert.strictEqual(toString(symbol), 'Symbol(a)');
-  });
+  it('should handle symbols', () => {
+    assert.strictEqual(toString(symbol), 'Symbol(a)')
+  })
 
-  it('should handle an array of symbols', function() {
-    assert.strictEqual(toString([symbol]), 'Symbol(a)');
-  });
+  it('should handle an array of symbols', () => {
+    assert.strictEqual(toString([symbol]), 'Symbol(a)')
+  })
 
-  it('should return the `toString` result of the wrapped value', function() {
-    var wrapped = _([1, 2, 3]);
-    assert.strictEqual(wrapped.toString(), '1,2,3');
-  });
-});
+  it('should return the `toString` result of the wrapped value', () => {
+    const wrapped = _([1, 2, 3])
+    assert.strictEqual(wrapped.toString(), '1,2,3')
+  })
+})

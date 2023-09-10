bf807f3aad73f0dcac4ad0f3c896f834a822225a
Jon Church <me@jonchurch.com>
2023-09-10 04:59:45 -0400

    remove IE specific test in isType-checks.test.js

--- test/isType-checks.test.js
+++ test/isType-checks.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import lodashStable from 'lodash'
-import { objToString, objectTag, _, xml } from './utils'
+import { objToString, objectTag, _} from './utils'
 
 describe('isType checks', () => {
   it('should return `false` for subclassed values', () => {
@@ -16,23 +16,6 @@ describe('isType checks', () => {
       const object = new Foo
       if (objToString.call(object) == objectTag) {
         assert.strictEqual(_[methodName](object), false, `\`_.${methodName}\` returns \`false\``)
-      }
-    })
-  })
-
-  it('should not error on host objects (test in IE)', () => {
-    const funcs = [
-      'isArguments', 'isArray', 'isArrayBuffer', 'isArrayLike', 'isBoolean',
-      'isBuffer', 'isDate', 'isElement', 'isError', 'isFinite', 'isFunction',
-      'isInteger', 'isMap', 'isNaN', 'isNil', 'isNull', 'isNumber', 'isObject',
-      'isObjectLike', 'isRegExp', 'isSet', 'isSafeInteger', 'isString',
-      'isUndefined', 'isWeakMap', 'isWeakSet'
-    ]
-
-    lodashStable.each(funcs, (methodName) => {
-      if (xml) {
-        _[methodName](xml)
-        assert.ok(true, `\`_.${methodName}\` should not error`)
       }
     })
   })

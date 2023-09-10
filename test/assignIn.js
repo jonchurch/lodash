b34697f5edceb80a4ddcbc96acca666ff47b3119
Jon Church <me@jonchurch.com>
2023-09-10 02:59:59 -0400

    remove assignIn and assignInWith test suites
    these functions were removed

--- test/assignIn.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import extend from '../extend.js'
-import assignIn from '../assignIn.js'
-
-describe('assignIn', () => {
-  it('should be aliased', () => {
-    assert.strictEqual(extend, assignIn)
-  })
-})
--- test/assignInWith.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import extendWith from '../extendWith.js'
-import assignInWith from '../assignInWith.js'
-
-describe('assignInWith', () => {
-  it('should be aliased', () => {
-    assert.strictEqual(extendWith, assignInWith)
-  })
-})

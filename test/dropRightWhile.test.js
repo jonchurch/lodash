6c38b8a360518aa64e3801916e6ffbb45620112a
Jon Church <me@jonchurch.com>
2023-09-10 15:28:23 -0400

    dropRightWhile.test.js, import _ from utils

--- test/dropRightWhile.test.js
+++ test/dropRightWhile.test.js
@@ -1,5 +1,5 @@
 import assert from 'assert'
-import { slice } from './utils'
+import { _, slice } from './utils'
 import dropRightWhile from '../dropRightWhile.js'
 
 describe('dropRightWhile', () => {

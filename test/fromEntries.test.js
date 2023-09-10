de94c947cc5cd96e5ccd48188fe04bb17501a566
Jon Church <me@jonchurch.com>
2023-09-10 16:02:14 -0400

    fromEntries.test.js, complete the rename of fromPairs to fromEntries

--- test/fromEntries.test.js
+++ test/fromEntries.test.js
@@ -1,13 +1,13 @@
 import assert from 'assert'
 import lodashStable from 'lodash'
 import { falsey, stubObject, square, isEven, LARGE_ARRAY_SIZE } from './utils'
-import fromPairs from '../fromEntries.js'
+import fromEntries from '../fromEntries.js'
 
-describe('fromPairs', () => {
+describe('fromEntries', () => {
   it('should accept a two dimensional array', () => {
     const array = [['a', 1], ['b', 2]],
       object = { 'a': 1, 'b': 2 },
-      actual = fromPairs(array)
+      actual = fromEntries(array)
 
     assert.deepStrictEqual(actual, object)
   })
@@ -17,7 +17,7 @@ describe('fromPairs', () => {
 
     const actual = lodashStable.map(falsey, (array, index) => {
       try {
-        return index ? fromPairs(array) : fromPairs()
+        return index ? fromEntries(array) : fromEntries()
       } catch (e) {}
     })
 
@@ -25,20 +25,20 @@ describe('fromPairs', () => {
   })
 
   it('should not support deep paths', () => {
-    const actual = fromPairs([['a.b', 1]])
+    const actual = fromEntries([['a.b', 1]])
     assert.deepStrictEqual(actual, { 'a.b': 1 })
   })
 
   it('should support consuming the return value of `Object.entries`', () => {
     const object = { 'a.b': 1 }
-    assert.deepStrictEqual(fromPairs(Object.entries(object)), object)
+    assert.deepStrictEqual(fromEntries(Object.entries(object)), object)
   })
 
   it('should work in a lazy sequence', () => {
     const array = lodashStable.times(LARGE_ARRAY_SIZE, (index) => [`key${index}`, index])
 
-    const actual = lodashStable(array).fromPairs().map(square).filter(isEven).take().value()
+    const actual = lodashStable(array).fromEntries().map(square).filter(isEven).take().value()
 
-    assert.deepEqual(actual, lodashStable.take(lodashStable.filter(lodashStable.map(fromPairs(array), square), isEven)))
+    assert.deepEqual(actual, lodashStable.take(lodashStable.filter(lodashStable.map(fromEntries(array), square), isEven)))
   })
 })

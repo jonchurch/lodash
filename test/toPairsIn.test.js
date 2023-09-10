f193f3c55de8f645abce7ffb876a37c0284808e4
Jon Church <me@jonchurch.com>
2023-09-10 14:17:44 -0400

    rm toPairsIn.test.js

--- test/toPairsIn.test.js
+++
@@ -1,9 +0,0 @@
-import assert from 'assert'
-import entriesIn from '../entriesIn.js'
-import toPairsIn from '../toPairsIn.js'
-
-describe('toPairsIn', () => {
-  it('should be aliased', () => {
-    assert.strictEqual(entriesIn, toPairsIn)
-  })
-})

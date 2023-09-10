cabb7f23f1b729962ae2c73fa4fb2dda6427846a
Jon Church <me@jonchurch.com>
2023-09-10 16:46:06 -0400

    initial.test.js, import _ from utils

--- test/initial.test.js
+++ test/initial.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert'
 import lodashStable from 'lodash'
-import { falsey, stubArray, LARGE_ARRAY_SIZE } from './utils'
+import { _, falsey, stubArray, LARGE_ARRAY_SIZE, isEven } from './utils'
 import initial from '../initial.js'
 
 describe('initial', () => {

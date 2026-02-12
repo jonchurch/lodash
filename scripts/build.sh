05d5e84c07ad5f2568fed90d68a40d3850ec654e
Jon Church <me@jonchurch.com>
2026-02-11 19:15:34 -0500

    temp: point build.sh at fork for testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,11 @@ REF="${1:-main}"
 SRC=$(mktemp)
 trap "rm -f $SRC" EXIT
 
+# TODO: change back to lodash/lodash before merging upstream
+REPO="jonchurch/lodash"
+
 echo "Fetching lodash.js (ref: $REF)..."
-curl -sfL "https://raw.githubusercontent.com/lodash/lodash/${REF}/lodash.js" -o "$SRC"
+curl -sfL "https://raw.githubusercontent.com/${REPO}/${REF}/lodash.js" -o "$SRC"
 
 npx lodash-cli modularize exports=amd --source "$SRC" -o ./   # AMD modules
 npx lodash-cli exports=amd -d --source "$SRC" -o ./main.js    # monolith in AMD wrapper

0cdeca5e48433d7aade37e0550af83772873e3e9
Jon Church <me@jonchurch.com>
2026-04-01 17:34:32 -0400

    build: use upstream lodash/lodash and lodash-archive/lodash-cli
    - build.sh: use --repo flag with SHA resolution instead of curl
    - lodash-cli: point at lodash-archive/lodash-cli@88630b7

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,16 @@
 set -euo pipefail
 
 REF="${1:-main}"
-SRC=$(mktemp)
-trap "rm -f $SRC" EXIT
 
-# TODO: change back to lodash/lodash before merging upstream
-REPO="jonchurch/lodash"
+# Resolve ref to a SHA so all CLI calls build from the same commit
+SHA=$(git ls-remote https://github.com/lodash/lodash.git "$REF" | cut -f1)
+if [ -z "$SHA" ]; then
+  # Assume it's already a SHA if ls-remote didn't match
+  SHA="$REF"
+fi
 
-echo "Fetching lodash.js (ref: $REF)..."
-curl -sfL "https://raw.githubusercontent.com/${REPO}/${REF}/lodash.js" -o "$SRC"
+REPO_REF="lodash/lodash#${SHA}"
+echo "Building from lodash/lodash @ ${SHA} (ref: ${REF})..."
 
-npx lodash-cli modularize exports=amd --source "$SRC" -o ./   # AMD modules
-npx lodash-cli exports=amd -d --source "$SRC" -o ./main.js    # monolith in AMD wrapper
+npx lodash-cli modularize exports=amd --repo "$REPO_REF" -o ./   # AMD modules
+npx lodash-cli exports=amd -d --repo "$REPO_REF" -o ./main.js    # monolith in AMD wrapper
